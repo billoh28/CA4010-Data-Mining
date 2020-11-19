f98af55aa9396f259e7ab2aed99861ab89eb13d0
billoh28 <ohanlonbill1998@gmail.com>
2020-11-19 21:15:57 +0000

    Adding barchart

---
+++ bar_chart.py
@@ -0,0 +1,23 @@
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Load in fight data
+fight_data = pd.read_csv(os.path.join("UFCDataset", "Original", "raw_total_fight_data.csv"), sep=';')
+
+# Remove nans
+fight_data = fight_data.dropna(axis=0, how='any')
+
+print(fight_data["win_by"].value_counts())
+
+# Make a bar chart of all the different decisions and their counts
+# fight_data["win_by"]
+
+data = fight_data["win_by"].value_counts()
+data = data.apply(lambda x: int(x))
+
+sns.barplot(x=data, y=data.index, palette='rocket_r')
+
+plt.show()
--- box_plot.py
+++ box_plot.py
@@ -1,4 +1,4 @@
-import os, math
+import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
