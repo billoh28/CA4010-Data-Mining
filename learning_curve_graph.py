ef828bd20415283d2212d136e7ccb94629919f0f
Killian Connolly <kcpconnolly@gmail.com>
2020-11-21 15:35:38 +0000

    Learning curve

---
+++ learning_curve_graph.py
@@ -0,0 +1,57 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.datasets import load_digits
+from sklearn.model_selection import learning_curve
+from sklearn.model_selection import ShuffleSplit
+from sklearn.ensemble import RandomForestClassifier
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from ufc_model import sanitation
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder
+from sklearn import metrics
+
+import sys
+
+args = sys.argv[1:]
+
+if int(args[1]) == 1:
+    fight_dataset = sanitation(int(args[0]), True)
+
+else:
+    fight_dataset = sanitation(int(args[0]))
+
+le = LabelEncoder()
+
+fight_dataset = fight_dataset.apply(le.fit_transform)
+
+X = fight_dataset.iloc[:, :-1].values
+y = fight_dataset.iloc[:, -1].values
+
+sizes, training_scores, testing_scores = learning_curve(RandomForestClassifier(max_depth=2, random_state=0), X, y, cv=10, scoring='accuracy', train_sizes=np.linspace(0.01, 1.0, 50)) 
+  
+# Mean and Standard Deviation of training scores 
+mean_training = np.mean(training_scores, axis=1) 
+Standard_Deviation_training = np.std(training_scores, axis=1) 
+  
+# Mean and Standard Deviation of testing scores 
+mean_testing = np.mean(testing_scores, axis=1) 
+Standard_Deviation_testing = np.std(testing_scores, axis=1) 
+  
+# dotted blue line is for training scores and green line is for cross-validation score 
+plt.plot(sizes, mean_training, '--', color="b",  label="Training score") 
+plt.plot(sizes, mean_testing, color="g", label="Cross-validation score") 
+  
+# Drawing plot 
+plt.title("LEARNING CURVE FOR KNN Classifier") 
+plt.xlabel("Training Set Size"), plt.ylabel("Accuracy Score"), plt.legend(loc="best") 
+plt.tight_layout() 
+plt.show()
